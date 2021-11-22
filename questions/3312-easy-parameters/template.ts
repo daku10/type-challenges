d1bcf904487c65a21b6ecb09f0affa8708058605
daku10 <daku.two@gmail.com>
2021-11-23 08:49:03 +0900

    solve 3312-easy-parameters

--- questions/3312-easy-parameters/template.ts
+++ questions/3312-easy-parameters/template.ts
@@ -1,1 +1,5 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => unknown> = T extends (
+  ...args: infer S
+) => unknown
+  ? S
+  : never
