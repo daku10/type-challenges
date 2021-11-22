db107d411f91acad809b1a96d87cbd2fa8dc213f
daku10 <daku.two@gmail.com>
2021-11-22 21:19:18 +0900

    solve 898-easy-includes, but Equal is difficult to understand...

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,11 @@
-type Includes<T extends readonly any[], U> = any;
+type MyEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+
+type Includes<T extends readonly any[], U> = T extends [infer R, ...infer S]
+  ? MyEqual<R, U> extends true
+    ? true
+    : Includes<S, U>
+  : false
