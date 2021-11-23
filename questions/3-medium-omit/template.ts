604aed8dd1b7fe3a6974d01cc406cc50dcfd3990
daku10 <daku.two@gmail.com>
2021-11-23 17:09:47 +0900

    add another solution of 3-medium-omit

--- questions/3-medium-omit/template.ts
+++ questions/3-medium-omit/template.ts
@@ -3,3 +3,8 @@ type MyExcludeForOmit<T, U extends T> = T extends U ? never : T
 type MyOmit<T, K extends keyof T> = {
   [P in MyExcludeForOmit<keyof T, K>]: T[P]
 }
+
+// another solution. but why this is success? what is as?
+// type MyOmit<T, K extends keyof T> = {
+//     [P in keyof T as P extends K ? never : P]: T[P]
+//   }
