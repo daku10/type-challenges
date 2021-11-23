b0266126d49922b3da43ddd2d0d8b75832472adf
daku10 <daku.two@gmail.com>
2021-11-23 22:04:03 +0900

    solve 8-medium-readonly-2, a bit difficult

--- questions/8-medium-readonly-2/template.ts
+++ questions/8-medium-readonly-2/template.ts
@@ -1,1 +1,10 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P]
+} & T
+
+// another solution
+// but K extends keyof T fails..., [K] extends [keyof T] succeeds.
+// why?
+// type MyReadonly2<T, K = ''> = [K] extends [keyof T]
+//   ? Readonly<Pick<T, K>> & Omit<T, K>
+//   : Readonly<T>
