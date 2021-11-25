4b27c0f21bd1c7594d18c314a9e56ed0c02a5d8a
daku10 <daku.two@gmail.com>
2021-11-26 00:31:19 +0900

    solve 12-medium-chainable-options, I have learned type generics of function

--- questions/12-medium-chainable-options/template.ts
+++ questions/12-medium-chainable-options/template.ts
@@ -1,4 +1,17 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+// I learned type generics of function
+// my first answer...
+// type Chainable<T = {}> = {
+//   option(
+//     key: string,
+//     value: any
+//   ): Chainable<T & { key: typeof value }>
+//   get(): T
+// }
+
+type Chainable<T = {}> = {
+  option<K extends string, V>(
+    key: K,
+    value: V
+  ): Chainable<T & { [key in K]: V }>
+  get(): T
 }
