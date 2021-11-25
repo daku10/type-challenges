e2f59f71dd86be8e8fa94f8054153aa435bd4092
daku10 <daku.two@gmail.com>
2021-11-25 23:31:29 +0900

    add another solution

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,5 +1,7 @@
 type DeepReadonly<T> = T extends object
   ? {
       readonly [P in keyof T]: T[P] extends () => {} ? T[P] : DeepReadonly<T[P]>
+      // use Built-in Function type is better.
+      // readonly [P in keyof T]: T[P] extends Function ? T[P] : DeepReadonly<T[P]>
     }
   : T
