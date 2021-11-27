001333dcaaf5696123b5e50fedd55ffbfdee59bd
daku10 <daku.two@gmail.com>
2021-11-27 15:49:07 +0900

    add interesting solution of 15-medium-last

--- questions/15-medium-last/template.ts
+++ questions/15-medium-last/template.ts
@@ -1,1 +1,5 @@
 type Last<T extends any[]> = T extends [...any, infer R] ? R : never
+// interesting solution, typescript can solve static type of below, R[length] means T[length] - 1, and so on.
+// type Last<T extends any[]> = T extends [any, ...infer R]
+//   ? T[R['length']]
+//   : never
