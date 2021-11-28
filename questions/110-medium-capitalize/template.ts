2b5387670df7130724d6f1454a3f732c993da6b6
daku10 <daku.two@gmail.com>
2021-11-28 19:25:59 +0900

    add another solution 110-medium-capitalize

--- questions/110-medium-capitalize/template.ts
+++ questions/110-medium-capitalize/template.ts
@@ -59,3 +59,40 @@ type CapitalizeChar<S extends string> = S extends 'a'
 type MyCapitalize<S extends string> = S extends `${infer T}${infer U}`
   ? `${CapitalizeChar<T>}${U}`
   : S
+
+// another solution
+// use map type
+// type UpperCaseMap = {
+//   a: 'A'
+//   b: 'B'
+//   c: 'C'
+//   d: 'D'
+//   e: 'E'
+//   f: 'F'
+//   g: 'G'
+//   h: 'H'
+//   i: 'I'
+//   j: 'J'
+//   k: 'K'
+//   l: 'L'
+//   m: 'M'
+//   n: 'N'
+//   o: 'O'
+//   p: 'P'
+//   q: 'Q'
+//   r: 'R'
+//   s: 'S'
+//   t: 'T'
+//   u: 'U'
+//   v: 'V'
+//   w: 'W'
+//   x: 'X'
+//   y: 'Y'
+//   z: 'Z'
+// }
+
+// type MyCapitalize<S extends string> = S extends `${infer T}${infer U}`
+//   ? T extends keyof UpperCaseMap
+//     ? `${UpperCaseMap[T]}${U}`
+//     : S
+//   : S
