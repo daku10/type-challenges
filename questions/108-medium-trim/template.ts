755e070199496cd58ef98e72e325af04459b34dc
daku10 <daku.two@gmail.com>
2021-11-28 18:55:01 +0900

    solve 108-medium-trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,5 @@
-type Trim<S extends string> = any
+type ReverseStr<T extends string> = T extends `${infer S}${infer U}`
+  ? `${ReverseStr<U>}${S}`
+  : T
+
+type Trim<S extends string> = ReverseStr<TrimLeft<ReverseStr<TrimLeft<S>>>>
