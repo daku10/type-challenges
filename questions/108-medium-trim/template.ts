d5800256e5da9a7e86139c0156f0ec95cfc512d8
daku10 <daku.two@gmail.com>
2021-11-28 19:07:59 +0900

    add another solution 108-medium-trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -2,4 +2,12 @@ type ReverseStr<T extends string> = T extends `${infer S}${infer U}`
   ? `${ReverseStr<U>}${S}`
   : T
 
-type Trim<S extends string> = ReverseStr<TrimLeft<ReverseStr<TrimLeft<S>>>>
+// type Trim<S extends string> = ReverseStr<TrimLeft<ReverseStr<TrimLeft<S>>>>
+
+// another solution
+// use infer and static types, clever solution
+type Trim<S extends string> = S extends
+  | `${infer T}${TrimTarget}`
+  | `${TrimTarget}${infer T}`
+  ? Trim<T>
+  : S
