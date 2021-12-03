5936b4f14a2ca70330126d43dd53f0dfd4dfe30f
daku10 <daku.two@gmail.com>
2021-12-03 15:57:17 +0900

    solve 296-medium-permutation

--- questions/296-medium-permutation/template.ts
+++ questions/296-medium-permutation/template.ts
@@ -1,1 +1,11 @@
-type Permutation<T> = any
+type Permutation<T, U = T> = [T] extends [never]
+  ? []
+  : T extends T
+  ? [T, ...Permutation<Exclude<U, T>>]
+  : never
+
+// below is basic idea.
+// T extends T means fixes T as one of T
+// type TwoElementUnionToArray<T, U = T> = T extends T ? [T, Exclude<U, T>] : never
+// type H = TwoElementUnionToArray<'A' | 'B'>
+// => H = ['A', 'B'] | ['B' , 'A']
