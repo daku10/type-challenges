d199430f205be65aecf383d4671032860b95d0f6
daku10 <daku.two@gmail.com>
2021-12-06 20:33:40 +0900

    solve 610-medium-camelcase

--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,34 @@
-type CamelCase<S> = any;
+type LowerAlphabet =
+  | 'a'
+  | 'b'
+  | 'c'
+  | 'd'
+  | 'e'
+  | 'f'
+  | 'g'
+  | 'h'
+  | 'i'
+  | 'j'
+  | 'k'
+  | 'l'
+  | 'm'
+  | 'n'
+  | 'o'
+  | 'p'
+  | 'q'
+  | 'r'
+  | 's'
+  | 't'
+  | 'u'
+  | 'v'
+  | 'w'
+  | 'x'
+  | 'y'
+  | 'z'
+
+type CamelCase1<S extends string> =
+  S extends `${infer T}-${infer U}${infer Rest}`
+    ? U extends LowerAlphabet
+      ? `${T}${Capitalize<U>}${CamelCase1<Rest>}`
+      : `${T}-${CamelCase1<`${U}${Rest}`>}`
+    : S
