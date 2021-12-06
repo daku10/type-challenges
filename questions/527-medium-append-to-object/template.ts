69a88430e8227cce97c1afefa0d918e62d40abff
daku10 <daku.two@gmail.com>
2021-12-06 16:06:08 +0900

    solve 527-medium-append-to-object

--- questions/527-medium-append-to-object/template.ts
+++ questions/527-medium-append-to-object/template.ts
@@ -1,1 +1,11 @@
-type AppendToObject<T, U, V> = any
+type Flat<T> = {
+  [key in keyof T]: T[key]
+}
+
+type AppendToObject<T, U extends string, V> = Flat<
+  {
+    [key in U]: V
+  } & {
+    [key in keyof T]: T[key]
+  }
+>
