59002c49d54312687c06ca77c0111fea9c612613
daku10 <daku.two@gmail.com>
2021-12-06 16:10:16 +0900

    add another solution 527-medium-append-to-object

--- questions/527-medium-append-to-object/template.ts
+++ questions/527-medium-append-to-object/template.ts
@@ -2,6 +2,7 @@ type Flat<T> = {
   [key in keyof T]: T[key]
 }
 
+// in test-cases.ts, use Equal type which can not deal with intersection type
 type AppendToObject<T, U extends string, V> = Flat<
   {
     [key in U]: V
@@ -9,3 +10,8 @@ type AppendToObject<T, U extends string, V> = Flat<
     [key in keyof T]: T[key]
   }
 >
+
+// another solution
+// type AppendToObject<T, U extends string, V> = {
+//     [key in keyof T | U]: key extends keyof T ? T[key] : V
+// }
