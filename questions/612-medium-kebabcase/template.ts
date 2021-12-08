9ec8716ed27c8992b108a573dcab0d15189f8cb0
daku10 <daku.two@gmail.com>
2021-12-08 23:28:12 +0900

    solve 612-medium-kebabcase

--- questions/612-medium-kebabcase/template.ts
+++ questions/612-medium-kebabcase/template.ts
@@ -1,1 +1,39 @@
-type KebabCase<S> = any;
+type UpperAlphabet =
+  | 'A'
+  | 'B'
+  | 'C'
+  | 'D'
+  | 'E'
+  | 'F'
+  | 'G'
+  | 'H'
+  | 'I'
+  | 'J'
+  | 'K'
+  | 'L'
+  | 'M'
+  | 'N'
+  | 'O'
+  | 'P'
+  | 'Q'
+  | 'R'
+  | 'S'
+  | 'T'
+  | 'U'
+  | 'V'
+  | 'W'
+  | 'X'
+  | 'Y'
+  | 'Z'
+
+type KebabCaseInternal<S extends string> = S extends `${infer T}${infer Rest}`
+  ? T extends UpperAlphabet
+    ? `-${Lowercase<T>}${KebabCaseInternal<Rest>}`
+    : `${T}${KebabCaseInternal<Rest>}`
+  : S
+
+type KebabCase<S extends string> = S extends `${infer First}${infer Rest}`
+  ? First extends UpperAlphabet
+    ? `${Lowercase<First>}${KebabCaseInternal<Rest>}`
+    : `${First}${KebabCaseInternal<Rest>}`
+  : S
