4225d3ffa87c53356395cf6830584d31ea8857c0
daku10 <daku.two@gmail.com>
2021-12-12 20:56:30 +0900

    add better solution 1097-medium-isunion

--- questions/1097-medium-isunion/template.ts
+++ questions/1097-medium-isunion/template.ts
@@ -1,5 +1,12 @@
 type IsUnion<T, U = T, isFirst = false> = [T] extends [never]
   ? false
   : T extends U
-  ? IsUnion<Exclude<U, T>, T, true>
+  ? IsUnion<Exclude<U, T>, never, true>
   : isFirst
+
+// better solution
+// type IsUnion<T, U = T> = T extends T
+//   ? U | T extends U & T
+//     ? false
+//     : true
+//   : never
