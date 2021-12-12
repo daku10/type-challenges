b531b5408ba908d438f846c6a722f1bb16412d47
daku10 <daku.two@gmail.com>
2021-12-12 16:45:54 +0900

    solve 949-medium-anyof

--- questions/949-medium-anyof/template.ts
+++ questions/949-medium-anyof/template.ts
@@ -1,1 +1,15 @@
-type AnyOf<T extends readonly any[]> = any;
+type AnyOf<T extends readonly any[]> = T extends []
+  ? false
+  : T extends [infer First, ...infer Rest]
+  ? First extends 0
+    ? AnyOf<Rest>
+    : First extends false
+    ? AnyOf<Rest>
+    : First extends []
+    ? AnyOf<Rest>
+    : MyEqual<First, {}> extends true
+    ? AnyOf<Rest>
+    : First extends ''
+    ? AnyOf<Rest>
+    : true
+  : never
